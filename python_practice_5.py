a04933f7360cc9e25d987d3ba56d7f3f7eea0e18
Mason Scarborough <masonwhispi@gmail.com>
2025-02-26 20:38:08 +0000

    bad code

---
+++ python_practice_5.py
@@ -0,0 +1,12 @@
+menu = {"Pizza": 1.99, "Soda": 0.69, "Double Chunk Chocolate Chip Cookie": 2.49}
+
+def add_item(item, price):
+    menu = [item] = price
+
+new_item = input("add an item: ")
+new_item_price = float(input("chose a price for your item: "))
+
+add_item(item, price)
+
+
+print(menu)
